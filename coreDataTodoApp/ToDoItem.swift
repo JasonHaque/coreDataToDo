4698bbbebec7601d533fd6c8c3339bdbaface1aa
JasonHaque <zahinjason220434@gmail.com>
2020-08-12 12:48:45 +0600

    added entity in core data

---
+++ coreDataTodoApp/ToDoItem.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoItem.swift
+//  coreDataTodoApp
+//
+//  Created by Sanviraj Zahin Haque on 12/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class ToDoItem:NSManagedObject,Identifiable{
+    
+    
+    
+}
