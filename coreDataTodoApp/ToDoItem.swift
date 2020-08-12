ed713b5b9f90e9018bdcf5003833b7e090a1ff64
JasonHaque <zahinjason220434@gmail.com>
2020-08-12 12:56:14 +0600

    added core data swift class

--- coreDataTodoApp/ToDoItem.swift
+++ coreDataTodoApp/ToDoItem.swift
@@ -11,6 +11,20 @@ import CoreData
 
 public class ToDoItem:NSManagedObject,Identifiable{
     
-    
+    @NSManaged public var createdAt : Date?
+    @NSManaged public var title : String?
     
 }
+
+extension ToDoItem{
+    
+    static func getAllToDoItem()-> NSFetchRequest<ToDoItem>{
+        let request : NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as! NSFetchRequest<ToDoItem>
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
+
