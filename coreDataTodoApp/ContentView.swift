c7a4707b7bbeda2ad3d248cdb26758345554d057
JasonHaque <zahinjason220434@gmail.com>
2020-08-12 12:39:27 +0600

    init

---
+++ coreDataTodoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coreDataTodoApp
+//
+//  Created by Sanviraj Zahin Haque on 12/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
